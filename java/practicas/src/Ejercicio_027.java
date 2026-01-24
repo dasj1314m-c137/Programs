df33068696d85f722a38f4bc5b761076f2459a67
dasj <dasj1314m@gmail.com>
2026-01-23 21:31:26 -0600

    ejercicio 027: calculo de valor absoluto, el cual es la distancia de un numero hacia el cero el valor siempre sera positivo

---
+++ java/practicas/src/Ejercicio_027.java
@@ -0,0 +1,7 @@
+class Ejercicio_027 {
+    public static void main(String[] args) {
+        double valor_absoluto;
+        valor_absoluto = -1* Math.round(-3.2);
+        System.out.println(valor_absoluto);
+    }
+}
---
+++ python/practicas/ejercicio_027.py
@@ -0,0 +1,2 @@
+valor_absoluto = -1 * round(-3.2)
+print(valor_absoluto)

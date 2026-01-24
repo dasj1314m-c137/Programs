0b2bfb0bc4cc5cf6616b0faacafb5ee8792c1163
dasj <dasj1314m@gmail.com>
2026-01-23 21:34:01 -0600

    ejercicio 028: convertir a string el resultado de una division redondeado con 3 decimales

---
+++ java/practicas/src/Ejercicio_028.java
@@ -0,0 +1,7 @@
+class Ejercicio_028 {
+    public static void main(String[] args) {
+        String rdo_division;
+        rdo_division = Double.toString(Math.round((5011.0/10000.0) * 1000.0 ) / 1000.0);
+        System.out.println(rdo_division);
+    }
+}
---
+++ python/practicas/ejercicio_028.py
@@ -0,0 +1,2 @@
+rdo_division = str(round(5011 / 10000, 3))
+print(rdo_division)

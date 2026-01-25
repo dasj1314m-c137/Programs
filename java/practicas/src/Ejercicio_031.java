a5c394eba8f2a55676597a1c4321717e3caafe76
dasj <dasj1314m@gmail.com>
2026-01-25 17:23:29 -0600

    ejercicio_031: en este ejercicio calculamos el area y perimetro de un cuadrado dado su lado que eran 3 metros

---
+++ java/practicas/src/Ejercicio_031.java
@@ -0,0 +1,8 @@
+class Ejercicio_031 {
+    public static void main(String[] args) {
+        int lado = 3, perimetro_cuadrado, area_cuadrado;
+        perimetro_cuadrado = lado * 4;
+        area_cuadrado = lado * lado;
+        System.out.printf("El lado del cuadrado es: %d metros, entonces su perimetro es: %d metros y su area es: %d metros cuadrados", lado, perimetro_cuadrado, area_cuadrado);
+    }
+}
---
+++ python/practicas/ejercicio_031.py
@@ -0,0 +1,5 @@
+lado = 3
+perimetro_cuadrado = lado * 4
+area_cuadrado = lado * lado
+
+print(f"El lado del cuadrdo es: {lado} metros, entonces el perimetro es: {perimetro_cuadrado} metros, y el area es: {area_cuadrado} metros²")

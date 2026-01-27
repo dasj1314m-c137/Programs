df0449ac37f8dbff36feb60b35f05b14a77331cf
dasj <dasj1314m@gmail.com>
2026-01-27 10:50:02 -0600

    ejercicio_035: en este recibimos la entrada de un usuario con las medidas de los lados de un rectangulo y calculamos su area y perimetro

---
+++ python/practicas/ejercicio_035.py
@@ -0,0 +1,5 @@
+l1 = float(input("Ingrese el lado menor del rectangulo: "))
+l2 = float(input("Ingrese el lado mayor del rectangulo: "))
+area = l1 * l2
+perimetro = l1 * 2 + l2 * 2
+print(f"El perimetro del rectangulo es: {perimetro} y el area es: {area}")

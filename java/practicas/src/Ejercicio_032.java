0aabcc6b6758fdff1bedb5bac13435a54944da1e
dasj <dasj1314m@gmail.com>
2026-01-27 10:33:33 -0600

    ejercicio_032: en este ejercicio teniamos que calcular el area de un triangulo dado su base y altura

---
+++ java/practicas/src/Ejercicio_032.java
@@ -0,0 +1,7 @@
+class Ejercicio_032 {
+    public static void main(String[] args) {
+        double base = 3, altura = 5, area;
+        area = (base * altura) / 2;
+        System.out.printf("La area del triangulo es: %.2f metros cuadrados", area);
+    }
+}
---
+++ python/practicas/ejercicio_032.py
@@ -0,0 +1,4 @@
+base = 3
+altura = 5
+area = (base * altura) / 2
+print(f"El area de triangulo es: {area}m²")

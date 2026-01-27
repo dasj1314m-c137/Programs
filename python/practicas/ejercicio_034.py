1f57d6a92d23139f542fed123f4977092c30b5f4
dasj <dasj1314m@gmail.com>
2026-01-27 10:46:34 -0600

    ejercicio_034: en este ejercicio recibimos la medida de lado de un cuadrado por parte del usuario y calculamos el area y el perimetro del cuadrado

---
+++ python/practicas/ejercicio_034.py
@@ -0,0 +1,4 @@
+lado = float(input("Ingresa la medida del lado del cuadrado a calcular area y perimetro: "))
+area = lado * lado
+perimetro = lado * 4
+print(f"El area del cuadrado es: {round(area, 2)} y el perimetro es: {perimetro}")

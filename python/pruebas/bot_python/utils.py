601ea1e769e7081f2bc1b1062410864a12915505
dasj <dasj1314m@gmail.com>
2026-05-07 08:52:15 -0600

    agregamos una funcion para crear los links que apuntan hacia headings [[file.md#Heading]], este recibe el nombre del archivo md y el heading al cual linkear

--- python/pruebas/bot_python/utils.py
+++ python/pruebas/bot_python/utils.py
@@ -15,6 +15,9 @@ def save_json_data(path, data):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
 
+def linkHeading_md(md_name, heading_name):
+    return f"[[{md_name}#{heading_name}]]"
+
 if __name__ == "__main__":
     list_test = ["item1", "item2", "item3"]
     list_view(list_test)

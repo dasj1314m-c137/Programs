310c408fe18b76f0322ed35bb774617364d33321
dasj <dasj1314m@gmail.com>
2026-05-07 09:06:37 -0600

    agregamos las funciones de añadir y eliminar pendiente ademas del control de estas mediante seleccion y tambien una cualidad para evitar o permiter que en la seleccion de opciones te pregunte por elegir o esta sea directo eleccion

--- python/pruebas/bot_python/bot.py
+++ python/pruebas/bot_python/bot.py
@@ -1,9 +1,4 @@
 import manager
-# import ask
-# import render
-# import search
-# import utils
-# import ask
 
 acts = ["Leer", "Entrenar box", "Meditar", "Editar videos", "Programar", "Escribir", "Aburrirse", "Jugar ajedrez", "Aprender"]
 
@@ -18,21 +13,23 @@ actions = {
         "path": r"/home/dasj/documents/works/obsidian_vault/diary/may.md",
         "json_key": "day_written"
     },
-    "dues": {
+    "show_dues": {
         "prompt": "Tus pendientes son: ",
         "path": r"/home/dasj/documents/works/obsidian_vault/dues/dues.md",
         "json_key": "dues_asked"
+    },
+    "modify_dues": {
+        "prompt": "¿Quieres agregar o eliminar un pendiente? ",
+        "path": r"/home/dasj/documents/works/obsidian_vault/dues/dues.md",
+        "json_key": "dues_added"
     }
-}
-
-paths = {
-    "homework": r"/home/dasj/documents/works/obsidian_vault/dues/homework/"
 }
 
 def main():
     manager.daily_check()
     manager.check_status_json(lambda: manager.match_response(actions["mood"]), actions["mood"])
     manager.check_status_json(lambda: manager.writing_files(actions["write_day"]), actions["write_day"])
-    # manager.duesMD_render(actions["dues"], paths["homework"])
+    manager.duesMD_render(actions["show_dues"])
+    manager.dues_manager(actions["modify_dues"])
 
 main()

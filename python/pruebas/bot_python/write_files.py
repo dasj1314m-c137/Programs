9a6d6f16dae0cd27bed73ce4ab9d3eebcdd8248e
dasj <dasj1314m@gmail.com>
2026-05-07 08:42:35 -0600

    agregamos funcion para eliminar un bloque de un archivo md, los argumentos que se pasan son el 'path', el cual es la direccion del archivo a eliminar, tambien esta el 'heading' el cual ayudara a encontrar el bloque que queremos eliminar, y el ultimo argumento es el 'delimiter' el cual ayuda de igual manera a determinar el bloque a eliminar el valor predeterminado de este argumento es '## ', esta funcion crea un archivo '.tmp' del archivo el cual vamos a eliminar un bloque, este se crea en la misma direccion que el archivo, luego escribimos en el archivo .tmp lo que queremos evitando lo que queremos eliminar basado en el 'heading' y el 'delimiter', luego al final, si no hubo errores, el contenido del archivo .tmp se remplaza al original y se elimina el archivo .tmp, si no hubo coincidencias eliminamos el tmp y no remplazamos nada

--- python/pruebas/bot_python/write_files.py
+++ python/pruebas/bot_python/write_files.py
@@ -1,5 +1,7 @@
 import json
 import utils
+import os
+from pathlib import Path
 
 def add_response(path, pattern, reponse):
     with open(path, 'a') as f:
@@ -14,3 +16,33 @@ def set_var_json(path, key1, key2, value):
         data = json.load(f)
     data[key1][key2] = value
     utils.save_json_data(path, data)
+
+def rm_MD_block(path, heading, delimiter="## "):
+    path = Path(path)
+    tmp_path = path.with_suffix('.tmp')
+    match = False
+    # print(f"Attempting to remove block with heading '{heading}' and delimiter '{delimiter}' from file: {path}")
+    try:
+        with path.open('r') as f_in, tmp_path.open('w') as f_out:
+            skip = False
+            for line in f_in:
+                if line.startswith(delimiter + heading):
+                    skip = True
+                    match = True
+                    continue
+                elif skip and line.startswith(delimiter):
+                    skip = False
+                    f_out.write(line)
+                    continue
+                if not skip:
+                    f_out.write(line)
+        if not match:
+            tmp_path.unlink()
+            return False
+        os.replace(tmp_path, path)
+        return True
+    except Exception as e:
+        print(f"Error occurred while removing MD block: {e}")
+        if tmp_path.exists():
+            tmp_path.unlink()
+        return None

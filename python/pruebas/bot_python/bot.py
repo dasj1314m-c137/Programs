3f5c607c24e3612185bf29fde2da8642a6104d84
dasj <dasj1314m@gmail.com>
2026-05-16 12:13:34 -0600

    agregamos dos caracteristicas a las actions las cuales son su funcion y si es pregunta directa

--- python/pruebas/bot_python/bot.py
+++ python/pruebas/bot_python/bot.py
@@ -1,4 +1,4 @@
-import manager
+import manager as mg
 
 acts = ["Leer", "Entrenar box", "Meditar", "Editar videos", "Programar", "Escribir", "Aburrirse", "Jugar ajedrez", "Aprender"]
 
@@ -6,30 +6,39 @@ actions = {
     "mood": {
         "prompt": "¿Como estas el dia de hoy? ",
         "path": r"data/mood_responses.txt",
-        "json_key": "mood_asked"
+        "json_key": "mood_asked",
+        "func": mg.match_response,
+        "dir_question": True
     },
     "write_day": {
         "prompt": "¿Quieres escribir tu dia? ",
         "path": r"/home/dasj/documents/works/obsidian_vault/diary/may.md",
-        "json_key": "day_written"
+        "json_key": "day_written",
+        "func": mg.writing_files,
+        "dir_question": False
     },
     "show_dues": {
-        "prompt": "Tus pendientes son: ",
+        "prompt": "¿Quieres revisar tus pendientes? ",
         "path": r"/home/dasj/documents/works/obsidian_vault/dues/dues.md",
-        "json_key": "dues_asked"
+        "json_key": "dues_shown",
+        "func": mg.duesMD_render,
+        "dir_question": False
     },
     "modify_dues": {
         "prompt": "¿Quieres agregar o eliminar un pendiente? ",
         "path": r"/home/dasj/documents/works/obsidian_vault/dues/dues.md",
-        "json_key": "dues_added"
+        "json_key": "dues_modified",
+        "func": mg.dues_manager,
+        "dir_question": False
     }
 }
 
 def main():
-    manager.daily_check()
-    manager.check_status_json(lambda: manager.match_response(actions["mood"]), actions["mood"])
-    manager.check_status_json(lambda: manager.writing_files(actions["write_day"]), actions["write_day"])
-    manager.duesMD_render(actions["show_dues"])
-    manager.dues_manager(actions["modify_dues"])
+    mg.daily_check()
+    mg.check_status_json(lambda: mg.match_response(actions["mood"]), actions["mood"])
+    mg.actions_question(actions["show_dues"])
+    mg.actions_question(actions["modify_dues"])
+    mg.actions_question(actions["write_day"])
+    mg.terminal_listening(actions)
 
 main()

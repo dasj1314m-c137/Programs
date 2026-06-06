f90e0ba744d4ac5646b2556a768df4f50be79ca8
dasj <dasj1314m@gmail.com>
2026-06-06 07:56:49 -0600

    creamos el servicio de audio y le pasamos la funcion listener del stream la cual guardara los bytes crudos que luego pasaremos en una funcion para que los una y guarde en el formato correcto para que lo pasemos al whisper, luego agregamos una una funcion init del manager la cual inicia la page, output_column, y el audio_recorder, en el manager para seleccioanarlo mas facil, pero este metodo es basura por las vars globales y muy wea, tratare de cambiarlo por algo mas limpio como un objeto que guarde estos datos, de ultimos añadimos el objeto grabador de audios en segundo plano

--- python/pruebas/bot_python/bot.py
+++ python/pruebas/bot_python/bot.py
@@ -1,12 +1,13 @@
 import manager as mg
 import flet as ft
 import render
+import flet_audio_recorder as far
 
 acts = ["Leer", "Entrenar box", "Meditar", "Editar videos", "Programar", "Escribir", "Aburrirse", "Jugar ajedrez", "Aprender"]
 
 actions = {
     "mood": {
-        "prompt": "¿Como estas el dia de hoy? ",
+        "prompt": "¿Como estas el dia de hoy?",
         "path": r"data/mood_responses.txt",
         "json_key": "mood_asked",
         "func": mg.match_response,
@@ -14,7 +15,7 @@ actions = {
     },
     "write_day": {
         "name": "Diario",
-        "prompt": "¿Quieres escribir tu dia? ",
+        "prompt": "¿Quieres escribir tu dia?",
         "path": "diary",
         "path_file": True,
         "json_key": "day_written",
@@ -23,7 +24,7 @@ actions = {
     },
     "show_dues": {
         "name": "Pendientes",
-        "prompt": "¿Quieres revisar tus pendientes? ",
+        "prompt": "¿Quieres revisar tus pendientes?",
         "path": "dues_file",
         "path_file": True,
         "json_key": "dues_shown",
@@ -32,12 +33,18 @@ actions = {
     },
     "modify_dues": {
         "name": "Pendientes",
-        "prompt": "¿Quieres agregar o eliminar un pendiente? ",
+        "prompt": "¿Quieres agregar o eliminar un pendiente?",
         "path": "dues_dir",
         "path_file": False,
         "json_key": "dues_modified",
         "func": mg.dues_manager,
         "dir_question": False
+    },
+    "book_learn": {
+        "name": "Aprendizajes libros",
+        "path": "books_dir",
+        "path_file": False,
+        "func": mg.book_learn,
     }
 }
 
@@ -51,14 +58,28 @@ async def main(page: ft.Page):
 
     # 2. Creamos un contenedor de texto que simulará nuestra "Terminal Visual"
     # Aquí es a donde mandaremos los prints en la Fase 2
-    terminal_output = ft.Column(scroll=ft.ScrollMode.ADAPTIVE, expand=True)
-    render.init_render(page, terminal_output) # Inicializamos render con la página y el widget de salida
+    terminal_output = ft.Column(
+        scroll=ft.ScrollMode.ADAPTIVE,
+        expand=True,
+        auto_scroll=True
+        )
 
+    audio_recorder = far.AudioRecorder(
+        configuration=far.AudioRecorderConfiguration(
+            encoder=far.AudioEncoder.PCM16BITS,
+            sample_rate=16000,
+            channels=1
+            ),
+            on_stream=lambda e: mg._audio_buffer.extend(e.chunk)
+    )
+
+    render.init_render(page, terminal_output) # Inicializamos render con la página y el widget de salida
+    mg.init_manager(page, terminal_output, audio_recorder) # Inicializamos manager con la página y el widget de salida
+    page.services.append(audio_recorder)
     # No agregamos el contenedor aquí, lo hacemos en main_menu para construir
     # una vista dividida con dos secciones (mensajes y opciones).
-
     # Mostrar menú principal con botones (reemplaza terminal_listening)
-    await mg.main_menu(page, actions, terminal_output)
+    await mg.main_menu(actions)
 
     mg.daily_check() # Verificar si es un nuevo día y resetear estados si es necesario
     await mg.check_status_json(actions["mood"])
